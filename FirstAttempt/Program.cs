e9f769e69a8890bfb2b6760a7a364e45495ae28f
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-07-27 12:40:58 +0100

    Update the Program Code

--- FirstAttempt/Program.cs
+++ FirstAttempt/Program.cs
@@ -7,6 +7,22 @@ namespace FirstAttempt
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int x = 100;
+            x += 10;
+
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(x);
+                x += i;
+            }
+
+            Console.WriteLine("Writing out command line arguments");
+
+            foreach(string item in args)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }

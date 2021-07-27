a3f120123a52f1f9ac2376f2ad7af2473a3d6692
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-07-27 11:44:30 +0100

    Add an edited version of the README file.

---
+++ FirstAttempt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstAttempt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CSharpIntroduction
+
+#### Information
+
+This is the **1st** time I have ever edited this README file using  _**Typora**_ .
+
